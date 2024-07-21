f5543bcc1b8356246a7956370777a293d431bf1a
Alex <larrybratton95@gmail.com>
2024-07-20 21:22:06 -0400

    removed unused functions

--- src/Render3D.h
+++ src/Render3D.h
@@ -3,8 +3,6 @@
 #include "components3D.h"
 namespace core
 {
-	flecs::system DrawCubes(flecs::world& ecs);
-	flecs::system DrawSpheres(flecs::world& ecs);
 	flecs::system DrawModels(flecs::world& ecs);
 	flecs::system DrawModelsInstanced(flecs::world& ecs);
 }
